3fb7f906db60fb0df6cab1e3aac1bfc0b89dc34f
mcodt1702 <marco@mcnyc.net>
2020-12-15 15:08:35 -0500

    api endpoint updates

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-  API_ENDPOINT:
-    "postgres://cpehpcfhnxhzgw:02b2ea45f905268ba3a9ebe28b36703616fd639431cce600a531a8bb42386f72@ec2-18-235-109-97.compute-1.amazonaws.com:5432/dive1lesmeqh0",
+  API_ENDPOINT: `https://limitless-shore-54406.herokuapp.com`,
   TOKEN_KEY: "auth-token",
   API_KEY: process.env.REACT_APP_API_KEY,
 };
